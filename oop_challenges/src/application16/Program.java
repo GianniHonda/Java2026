0ace374859408accec86dcc630b3d5825b0dddf8
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2026-02-20 18:21:02 +0900

    2-20

---
+++ oop_challenges/src/application16/Program.java
@@ -0,0 +1,21 @@
+package application16;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities16.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
+		
+		sc.close();
+	}
+
+}
